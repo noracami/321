e65f0bcfb21ee8bb1869b1b506c49d4bbba5c40e
Ker ker <kerke.2011@gmail.com>
2016-04-28 02:54:22 +0800

    first commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vigilantjourney.wsgi --log-file -
---
+++ vigilantjourney/settings/local.py
@@ -0,0 +1,20 @@
+# vigilantjourney/settings/local.py
+
+from .base import *     # noqa
+import dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'rg1hqqj(nxq9b5@fmjp-e%(e_@ksma8bhmpe+z^cw^64n=zi#i'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+    }
+}
