24377dabda059e005eec4fa6d999f89ff5dad202
Ker ker <kerke.2011@gmail.com>
2016-05-18 00:34:06 +0800

    active rest api for friend model

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from quickstart.models import Friend
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +13,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class FriendSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Friend
+        fields = ('name', 'mid', 'is_block', 'edit_time')
--- vigilantjourney/urls.py
+++ vigilantjourney/urls.py
@@ -22,6 +22,7 @@ from quickstart import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'friends', views.FriendViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
