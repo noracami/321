afa5d425b0777ad71755a6d0fce9c25aea739cba
Ker ker <kerke.2011@gmail.com>
2016-05-17 23:52:19 +0800

    record user who add the robot.

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Friend(models.Model):
+
+    name = models.CharField(max_length=30, default='username')
+    mid = models.CharField(max_length=40, primary_key=True)
+    is_block = models.BooleanField(default=False)
+    edit_time = models.DateTimeField('上次修改時間', auto_now=True, editable=False)
+
+    def __str__(self):
+        return '%s(%s)' % (self.name, self.mid)
+
+    def was_edited_recently(self):
+        return self.edit_time >= timezone.now() - datetime.timedelta(days=7)
